0e638db662a7e0bd0ec3b44b4be3587ae346ecb2
Arnav <arnjain3@gmail.com>
2023-11-26 16:02:17 +0530

    Initial Commit

---
+++ watter/watterApp.swift
@@ -0,0 +1,17 @@
+//
+//  watterApp.swift
+//  watter
+//
+//  Created by Arnav Jain on 26/11/23.
+//
+
+import SwiftUI
+
+@main
+struct watterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
